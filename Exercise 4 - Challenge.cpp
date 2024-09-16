736937123c55e7bbe974dd7932e1005f53395ab4
PolisticoThevins <qtlpolistico@tip.edu.ph>
2024-09-16 12:07:18 +0800

    Add files via upload

---
+++ Exercise 4 - Challenge.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main ()
+{
+	
+	int i;
+	int j= 10;
+	
+	for(i=4; i<j;i++)
+	{
+		cout << "Hello" << i << endl;
+		if (i==j)
+		{
+			break;
+		}
+	}
+}
